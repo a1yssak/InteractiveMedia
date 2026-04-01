3fa4beeb73768c20b603b90b88927bffff5710d0
a1yssak <lyss_kaz@hotmail.com>
2026-04-01 23:03:02 +1100

    initial commit

---
+++ html/script/script.js
@@ -0,0 +1,7 @@
+const customCursor = document.getElementById('custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+    customCursor.style.left = e.pageX + 'px';
+    customCursor.style.top = e.pageY + 'px';
+});
+

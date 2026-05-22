49607decd958611ee450049d651065103cfdc691
a1yssak <lyss_kaz@hotmail.com>
2026-05-22 20:20:22 +1000

    further edits

--- html/script/spinnyText.js
+++ html/script/spinnyText.js
@@ -1,31 +1,48 @@
-/*CODE USED FROM @karenanndonnachie GITHUB FILES!*/
+/*CODE SOURCED FROM @karenanndonnachie GITHUB FILES AND EDITED BY ME!*/
 var spin = 0;
-var letterSize = 24;
+var letterSize = 30;
+
+/*styling*/
+let ink = "#B00F64";
+let line = "#ebbbd1";
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(240);
-    fill(0);
     angleMode(DEGREES);
     textAlign(CENTER);
     frameRate(15);
+    /*background(0, 0);*/
+
+    createCanvas(windowWidth, windowHeight);
+    textFont('Annie Use Your Telescope');
+    angleMode(DEGREES);
+    textAlign(CENTER, CENTER);
 }
 
 function draw() {
-    fill(0); //text ink color!
+    clear();
 
     push();
-    translate(width / 2, height / 2);
-    rotate(spin);
+    translate(width / 2, height / 3.5);
+    /*rotate(spin);*/
     textSize(letterSize);
-    text('SPIN\nME ROUND', 0, -letterSize * 0.3);
-    spin += map(mouseX, 0, width, -10, 10); //this will make the mouseX affect the spin direction and speed
-    letterSize = map(mouseY, 0, height, 8, 72);
-    circle(0, 0, letterSize / 2);
-    noFill();
-    stroke(0);
-    circle(0, 0, letterSize * 10);
-    pop();
 
-    background(220, 51);
+    /*animation*/
+    spin += map(mouseX, 0, width, -1, 1);
+    letterSize = map(mouseY, 10, height, 20, 200);
+
+    /* circle! 
+    stroke(line);
+    strokeWeight(4);
+    noFill();
+    circle(0, 0, letterSize * 5);
+    circle(0, 0, letterSize * 25);*/
+
+    /* text styling */
+    fill(ink);
+    stroke(255);
+    strokeWeight(5);
+    text('my portfolio!', 0, -letterSize * 0.3);
+
+    pop();
 }
